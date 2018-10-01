cc25bb7b04d92006797a8600c87a42d0d357499c
Max Baird <mmb1@hw.ac.uk>
2018-10-01 20:21:16 +0100

    Updates
    Config has been updated to keep last checkpoint and the source has been
    modified to use the new macro FTI_Protect_kernel which takes an ID of a
    kernel to protect.

--- fti_cuda.cu
+++ fti_cuda.cu
@@ -188,7 +188,7 @@ int main(int argc, char *argv[])
 
   //vector_add<<<grid_size, block_size>>>(d_a, d_b, d_c, chunk_info.n_items);
   //FTI_KERNEL_LAUNCH(rank_id, 0.0001, vector_add, grid_size, block_size,0,0,d_a, d_b, d_c, chunk_info.n_items, rank_id);
-  FTI_KERNEL_LAUNCH(0.00001, vector_add, grid_size, block_size,0,0,d_a, d_b, d_c, chunk_info.n_items, rank_id);
+  FTI_Protect_Kernel(1, 0.00001, vector_add, grid_size, block_size,0,0,d_a, d_b, d_c, chunk_info.n_items, rank_id);
   KERNEL_ERROR_CHECK();
   CUDA_ERROR_CHECK(cudaDeviceSynchronize());
   
